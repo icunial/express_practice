01e49ad91fe91e5cb14f57e46d5ae64039852a45
Ignacio Cunial <igsecu@hotmail.com>
2023-08-05 18:08:13 -0300

    Created public folder - index.html

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  //res.send("Hello World!");
+  res.sendFile(path.join(__dirnamem, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;

994bd7b47b99c738d366dd1efab41c893e74d36a
Ignacio Cunial <igsecu@hotmail.com>
2023-08-06 17:49:34 -0300

    Created middleware folder - Created logger.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
-const moment = require("moment");
 const members = require("./Members");
+
+const logger = require("./middleware/logger");
 
 const app = express();
 
@@ -10,14 +11,8 @@ const app = express();
   res.sendFile(path.join(__dirname, "public", "index.html"));
 }); */
 
-// Create Middleware
-const logger = (req, res, next) => {
-  console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`);
-  next();
-};
-
 // Init middleware
-app.use(logger);
+//app.use(logger);
 
 // Gets all members
 app.get("/api/members", (req, res) => {

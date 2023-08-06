da1215f69df352979b0173d0e8ae0219eb3a013a
Ignacio Cunial <igsecu@hotmail.com>
2023-08-06 18:10:32 -0300

    Get single member route -> found method and status 404 not found!

--- index.js
+++ index.js
@@ -21,11 +21,19 @@ app.get("/api/members", (req, res) => {
 
 // Get single member
 app.get("/api/members/:id", (req, res) => {
-  res.json(
-    members.filter((member) => {
-      return member.id === parseInt(req.params.id);
-    })
-  );
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(
+      members.filter((member) => {
+        return member.id === parseInt(req.params.id);
+      })
+    );
+  } else {
+    res.status(404).json({
+      msg: `Member with the id: ${req.params.id} not found!`,
+    });
+  }
 });
 
 // Set static folder
